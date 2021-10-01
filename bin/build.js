f5fe097c8c0401527a74dac4014720d1ac802ccd
Giancarlo Palavicini <kasthor@gmail.com>
2021-10-01 12:11:41 -0600

    fix: fix builder

--- bin/build.js
+++ bin/build.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const DATA_FILE = "./src/data/cards.json";
-const CARD_FILES = "./src/assets/cards/";
+const CARD_FILES = "./public/cards/";
 const IGNORE = [".DS_Store"];
 
 const readDirectory = (base) => {
@@ -16,9 +16,9 @@ const readDirectory = (base) => {
         return { [f]: readDirectory(file) };
       } else if (IGNORE.includes(f)) {
         return null;
-      } else {
+      } 
         return path.basename(f, ".svg");
-      }
+      
     })
     .filter((i) => !!i);
 };
@@ -26,7 +26,7 @@ const readDirectory = (base) => {
 const build = () => {
   fs.writeFileSync(
     DATA_FILE,
-    JSON.stringify(readDirectory(CARD_FILES), null, 2)
+    JSON.stringify(Object.assign({}, ...readDirectory(CARD_FILES)), null, 2)
   );
 };
 
