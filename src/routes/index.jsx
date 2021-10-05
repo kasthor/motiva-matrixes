897a8ed415c8f83e55223bb40b2bc96c59b2a804
Giancarlo Palavicini <kasthor@gmail.com>
2021-10-04 18:00:20 -0600

    feat: hash history

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,10 @@
 import Router from "preact-router";
 import { Card } from "../pages/Card";
 import { Cover } from "../pages/Cover";
+import { createHashHistory } from "history"
 
 export const Routes = () => (
-  <Router>
+  <Router history={createHashHistory()}>
     <Card path="/card/:brand?/:category?" />
     <Cover path="/" />
   </Router>
